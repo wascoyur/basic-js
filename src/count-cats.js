67db17a6b623d412964c69dca0104ef47a57b071
wascoyur <mytem20007@gmail.com>
2020-09-25 15:36:40 +0500

    feat: count-cats ready

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(cats) {
+  // throw new CustomError('Not implemented');
+  let count = 0;
+  cats.forEach(row => {
+    // console.log('row:', row);
+    row.forEach((cell) =>{
+      if (cell/* .toString() */ === '^^') {
+        // console.log('cell:', cell);
+          count++;
+      }
+    })
+  });
+  // console.log('count:', count);
+  return count;
 };
