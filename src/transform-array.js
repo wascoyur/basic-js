9fda7a9282566aca4397ed49e2be556afe73da31
wascoyur <mytem20007@gmail.com>
2020-09-26 20:57:24 +0500

    fix:  test

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let outArr = [];
+    if(!Array.isArray(arr)) throw new Error;
+
+    outArr = arr.map((el, index, currArr) => {
+      if (el.includes('--double')) {
+        switch (el) {
+          case '--discard-next':
+            if(currArr[index + 1])
+            break;
+
+          default:
+            break;
+        }
+      }
+      return el;
+    })
+  return outArr;
 };
