004fb44a4f6e9d5a98232f4f06f2ebacc90b3246
wascoyur <mytem20007@gmail.com>
2020-09-27 16:41:36 +0500

    draft: new logic

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,21 +3,35 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function transform(arr) {
   // throw new CustomError('Not implemented');
   // remove line with error and write your code here
-  let outArr = [];
+  let outArr = Array.from(arr);
     if(!Array.isArray(arr)) throw new Error;
 
-    outArr = arr.map((el, index, currArr) => {
-      if (el.includes('--double')) {
-        switch (el) {
-          case '--discard-next':
-            if(currArr[index + 1])
-            break;
-
-          default:
-            break;
-        }
+  outArr = outArr.map((el, index, currArr) => {
+    if (el.toString().includes('--')) {
+      let itemOut = '';
+      switch (el) {
+        case '--discard-next':
+          if (currArr[index + 1]) {
+            currArr.splice(index, 2, '--', '--')
+          } else {
+            currArr.splice(index, 1, '--')
+          }
+          break;
+        case '--discard-prev':
+          if (currArr[index - 1]) {
+            currArr.splice(index - 1, 2, '--', '--')
+          } else {
+            currArr.splice(index, 1, '--')
+          }
+          break;
+        case '--double-next':
+          if (currArr[index + 1]) currArr.splice(index, 2,)
+          break;
+        case '--double-prev':
+          if (currArr[index - 1]) currArr.splice(index, 2)
+          break;
       }
-      return el;
-    })
-  return outArr;
+    }
+    return itemOut;
+  })
 };
